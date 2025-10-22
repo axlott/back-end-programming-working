2a34f174abac335e19c007f7d9c456fc8898f345
Axel Persan <axlpersan@gmail.com>
2025-10-22 17:48:26 -0400

    H: Create checkout REST controller

---
+++ src/main/java/com/example/demo/controllers/CheckoutController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.CheckoutService;
+import com.example.demo.services.CheckoutServiceImpl;
+import com.example.demo.services.Purchase;
+import com.example.demo.services.PurchaseResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/checkout")
+@CrossOrigin("http://localhost:4200")
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        return this.checkoutService.placeOrder(purchase);
+    }
+}
+
